61290f8ce1d77ce6ab375b5ac93784453e497fa8
jbozarov <tech@jbozarov.com>
2022-06-19 20:22:42 -0400

    First commit

---
+++ src/test/java/com/java/coding/JavaCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.coding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
